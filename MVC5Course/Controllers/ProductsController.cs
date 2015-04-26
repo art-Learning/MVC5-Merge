1ade5343cc4384a6015238f07a7c6b8bc0d9f8b4
art <partypeopleland@gmail.com>
2015-04-26 16:15:04 +0800

    修正 ProductsController 的 Edit 動作方法，改用延遲繫結的方式更新資料

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -77,11 +77,14 @@ namespace MVC5Course.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        public ActionResult Edit(int id,FormCollection form)
         {
-            if (ModelState.IsValid)
+            //改為延遲驗證
+            //if (ModelState.IsValid)
+            Product product = db.Product.Find(id);
+            if (TryUpdateModel<Product>(product, new string[] { "ProductName", "Price", "Stock" }))
             {
-                db.Entry(product).State = EntityState.Modified;
+                //db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
