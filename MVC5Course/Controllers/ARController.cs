dbcd168503801e4b227c99342050caa3bb2e39a4
art <partypeopleland@gmail.com>
2015-04-26 10:23:53 +0800

    練習 FileResult 的各種用法 ( 檔案路徑, 指定 Content-Type, byte[], 強制下載檔案 )

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult File1()
+        {
+            WebClient wc = new WebClient();
+            byte[] content = wc.DownloadData("http://www.imageshop.com.tw/pic/shop/home/men-world.jpg");
+            return File(content, "image/jpeg");
+        }
+        public ActionResult File2()
+        {
+            byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/1.jpg"));
+            return File(content, "image/jpeg");
+        }
+        public ActionResult File3()
+        {
+            WebClient wc = new WebClient();
+            byte[] content = wc.DownloadData("http://www.imageshop.com.tw/pic/shop/home/men-world.jpg");
+            return File(content, "image/jpeg", "man.jpg");
+        }
+        public ActionResult File4()
+        {
+            
+            //return File(Server.MapPath("~/Content/test.html"),"text/plain");
+            return File(Server.MapPath("~/Content/test.html"), "text/html");
+        }
+
+    }
+}
