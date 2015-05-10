fe9dd835abf8b6310964e122642cf19f4a539d26
art <partypeopleland@gmail.com>
2015-05-10 10:07:12 +0800

    修正Bundle的設定

--- MVC5Course/App_Start/BundleConfig.cs
+++ MVC5Course/App_Start/BundleConfig.cs
@@ -12,7 +12,6 @@ namespace MVC5Course
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.unobtrusive-ajax.js"
                         ));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
