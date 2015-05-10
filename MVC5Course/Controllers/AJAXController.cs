fefc9c9596a3a64b615cc3cdb190de6a1e014bd4
art <partypeopleland@gmail.com>
2015-05-10 09:49:29 +0800

    練習使用最基本的 Ajax helper 使用方式

---
+++ MVC5Course/Controllers/AJAXController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AJAXController : Controller
+    {
+        // GET: AJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetTime()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
